83c43f339e7cab138216a82d99b4a15852eda9d5
Alex Tyler <44123801+alxtyl@users.noreply.github.com>
2019-02-14 12:14:37 -0800

    Updated Comments

--- Homework 1/Points.java
+++ Homework 1/Points.java
@@ -1,26 +1,32 @@
 import java.awt.Point;
 /**
- * CSCI 114 Homework
+ * CSCI 114 Homework 1
  *
  * @Alex Tyler
- * @2/13/19
+ * @2/14/19
  */
 public class Points
 {
     public static void main(String[] args)
     {
+        //initializing points
         Point pointA = new Point(0,10);
         Point pointB = new Point(20,30);
 
+        //printing out points init vaules
         System.out.println("pointA is: " + pointA.toString());
         System.out.println("pointB is: " + pointB.toString());
  
+        //moving pointA location
         pointA.setLocation(20,30);
         
+        //printing out new point locations
         System.out.println("The new location of pointA is: " + pointA.toString());
         
+        //tranlating pointA by 70,50
         pointA.translate(70,50);
        
+        //printing out new location of pointA
         System.out.println("The new location of pointA after translation is: " + pointA.toString());
     }
 }
