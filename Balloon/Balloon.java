51446ed0c6bfca86da07ea2a462ed05ec011634a
Alex Tyler <44123801+alxtyl@users.noreply.github.com>
2019-03-05 13:08:08 -0800

    Added new Balloon Lab

---
+++ Balloon/Balloon.java
@@ -0,0 +1,52 @@
+
+/**
+ * Write a description of class Balloon here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Balloon
+{
+    // instance variables - replace the example below with your own
+    private String color;
+    private double size;
+    public final double PI = 3.1415926;
+
+    /**
+     * Constructor for objects of class Balloon
+     */
+    public Balloon(String userBalloonC, double userBalloonS)
+    {
+        // initialise instance variables
+        color = userBalloonC;
+        size = userBalloonS;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public double inflate(double inflateBalloon)
+    {
+        // put your code here
+        size *= inflateBalloon;
+        
+        return size;
+    }
+    
+    public String displayBalloonColor()
+    {
+        return color;
+    }
+    
+    public double balloonVolume()
+    {
+        double volume;
+        
+        volume = (4/3) * PI * (size * size * size);
+        
+        return volume;
+    }
+}
---
+++ Balloon/Tester.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+/**
+ * This class asks the user about the ballon they would like to create and then
+ * inflates it by 5.0 units, then outputs the color and size.
+ *
+ * @Alex Tyler
+ * @3/17/19
+ */
+public class Tester
+{
+    /**
+     * main() method
+     */
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter the color of the ballon:");
+        String userBalloonC = sc.nextLine(); //C stands for color
+        
+        System.out.println("Please enter the size of the ballon:");
+        double userBalloonS = sc.nextDouble(); //S stands for Size
+        
+        Balloon userBalloon = new Balloon(userBalloonC, userBalloonS); //creating new object
+        
+        userBalloon.inflate(5.0); //inflatung the ballon by 5.0
+        
+        System.out.println("For the " + userBalloon.displayBalloonColor() + " ballon");
+        System.out.printf("Volume is now: %.2f%n", userBalloon.balloonVolume());
+    }
+}
